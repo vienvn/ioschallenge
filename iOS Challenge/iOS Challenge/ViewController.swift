6edbb159a7e5651c00f0c08cfa2760180bc15cc3
Vien V.Nguyen <vien.nguyen@sutrixsolutions.com>
2016-06-21 17:08:39 +0700

    Milestone four

--- iOS Challenge/iOS Challenge/ViewController.swift
+++ iOS Challenge/iOS Challenge/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
   
-  var data: [UpcomingGuides] = []
+  var data: [[UpcomingGuides]] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,12 +20,35 @@ class ViewController: UIViewController {
     
     tableView.delegate = self
     tableView.dataSource = self
-//    tableView.registerClass(UpcomingTableViewCell.self, forCellReuseIdentifier: "cell")
     
     Service.sharedInstance.getUpcomingGuides { (data) in
-      self.data = data
+      self.data = self.groupUpcoming(data)
       self.tableView.reloadData()
     }
+  }
+  
+  func groupUpcoming(data: [UpcomingGuides]) -> [[UpcomingGuides]] {
+    let dataSorted = data.sort({ $0.startDate.compare($1.startDate) == NSComparisonResult.OrderedAscending })
+    
+    var list: [[UpcomingGuides]] = []
+    
+    var tmpDate = NSDate(timeIntervalSince1970: 0)
+    var tmpList: [UpcomingGuides] = []
+    
+    for item in dataSorted {
+      if item.startDate.compare(tmpDate) == NSComparisonResult.OrderedSame {
+        tmpList.append(item)
+      } else {
+        if tmpList.count != 0 {
+          list.append(tmpList)
+          tmpList = []
+        }
+        tmpList.append(item)
+      }
+      tmpDate = item.startDate
+    }
+    list.append(tmpList)
+    return list
   }
 
   override func didReceiveMemoryWarning() {
@@ -39,31 +62,44 @@ extension ViewController: UITableViewDelegate {
 }
 
 extension ViewController: UITableViewDataSource {
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return data.count
   }
   
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data[section].count
+  }
+  
+  func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "MMM d, yyyy"
+    
+    return "\(dateFormatter.stringFromDate((data[section][0].startDate)))"
+  }
+  
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
     let cell = tableView.dequeueReusableCellWithIdentifier("UpcomingCell")! as! UpcomingTableViewCell
     
+    
     if let nameLable = cell.viewWithTag(100) as? UILabel {
-      nameLable.text = data[indexPath.row].name
+      nameLable.text = data[indexPath.section][indexPath.row].name
     }
     
     if let cityLable = cell.viewWithTag(200) as? UILabel {
-      cityLable.text = data[indexPath.row].city
+      cityLable.text = data[indexPath.section][indexPath.row].city
     }
     
     if let stateLable = cell.viewWithTag(400) as? UILabel {
-      stateLable.text = data[indexPath.row].state
+      stateLable.text = data[indexPath.section][indexPath.row].state
     }
     
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "MMM d, yyyy"
+    let endDate = data[indexPath.section][indexPath.row].endDate!
     if let endLable = cell.viewWithTag(300) as? UILabel {
-      endLable.text = "\(data[indexPath.row].endDate!)"
+      endLable.text = "\(dateFormatter.stringFromDate(endDate))"
     }
-
-//    cell.textLabel?.text = data[indexPath.row].name
     
     return cell
   }
